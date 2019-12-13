2ece9f47b6246a65a0f2dfc7de4a3488fe3e57a2
Anton Lytiakov <lytyakov@gmail.com>
2019-12-13 12:16:29 +0300

    initial commit

---
+++ newsparser.py
@@ -0,0 +1,18 @@
+import json
+import requests
+from bs4 import BeautifulSoup as Soup 
+import sqlite3
+
+def parse_rss(url):
+
+    return
+
+
+def parse_news(url):
+
+    return
+
+
+if __name__ == "__main__":
+
+    pass
