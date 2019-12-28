c9cdd5f55803fe7d146663c12fd70179cd575961
Anton Lytiakov <lytyakov@gmail.com>
2019-12-28 22:45:13 +0400

    orm sql + newsparser

---
+++ app/parser/config.py
@@ -0,0 +1,138 @@
+import os
+
+DB_PATH =  os.path.join(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 
+    "news.db"
+)
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + DB_PATH
+
+PARSER_RULES = {
+    "ria": {
+        "url": "https://ria.ru/export/rss2/index.xml",
+        "rss": {
+            "title": "title", 
+            "description": "description", 
+            "url": "guid", 
+            "pub_dttm": "pubdate",
+        },
+        "news": {
+            "fulltext": {
+                "tag": "div",
+                "attrs": {
+                    "class": "article__text"
+                } 
+            },
+            "authors": {
+                "tag": "div",
+                "attrs": {
+                    "class": "article__author-name"
+                }
+            },
+            "keywords": {
+                "tag": "a",
+                "attrs": {
+                    "class": "article__tags-item"
+                },
+            },
+            "rubric": {
+                "tag": "meta",
+                "attrs": {
+                    "name": "analytics:rubric"
+                },
+                "get": "content"
+            }
+        }
+    },
+    "tass": {
+        "url": "http://tass.ru/rss/v2.xml",
+        "rss": {
+            "title": "title", 
+            "description": "description", 
+            "url": "guid", 
+            "pub_dttm": "pubdate",
+        },
+        "news": {
+            "fulltext": {
+                "tag": "p"
+            },
+            "authors": {
+                "tag": "div",
+                "attrs": {
+                    "class": "person-name"
+                } 
+            },
+            "keywords": {
+                "tag": "a",
+                "attrs": {
+                    "class": "tags__item"
+                }
+            }
+        }
+    },
+    "meduza": {
+        "url": "https://meduza.io/rss/all",
+        "rss": {
+            "title": "title", 
+            "description": "description", 
+            "url": "guid", 
+            "pub_dttm": "pubdate",
+        },
+        "news": {
+            "fulltext": {
+                "tag": "p",
+                "attrs": {"class": "SimpleBlock-p"}
+            },
+            "authors": {
+                "tag": "p",
+                "attrs": {"class": "MaterialNote-note_caption"}
+            },
+            "keywords": {
+                "tag": "meta",
+                "attrs": {"name": "keywords"},
+                "get": "content"
+            },
+            "rubric": {
+                "tag": "div",
+                "attrs": {
+                    "class": "Tag-root Tag-large Tag-gold"
+                }
+            }
+        }
+    },
+    "newsru": {
+        "url": "https://rss.newsru.com/all_news/",
+        "rss": {
+            "title": "title", 
+            "description": "description", 
+            "url": "guid", 
+            "pub_dttm": "pubdate",
+        },
+        "news": {
+            "fulltext": {
+                "tag": "p",
+                "attrs": {
+                    "class": "maintext"
+                }
+            },
+            "authors": {
+                "tag": "a",
+                "attrs": {
+                    "class": "article-author"
+                }
+            },
+            "keywords": {
+                "tag": "meta",
+                "attrs": {
+                    "name": "news_keywords"
+                },
+                "get": "content"
+            },
+            "rubric": {
+                "tag": "div",
+                "attrs": {
+                    "class": "main-caption"
+                }
+            }
+        }
+    }
+}
