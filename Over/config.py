b896c1a6a04b375c41e91bf7d849733dcc25babb
lytyakov <41022253+lytyakov@users.noreply.github.com>
2019-12-29 14:14:34 +0400

    resolve conflicts

--- Over/config.py
+++ Over/config.py
@@ -1,39 +1,138 @@
-DB_PATH =  "./news.db"
+import os
 
-TABLES = {
-    "rss": {
-        "id": "TEXT", 
-        "title": "TEXT", 
-        "description": "TEXT", 
-        "guid": "TEXT", 
-        "pubdate": "TEXT", 
-        "etl_dttm": "TEXT"
+DB_PATH =  os.path.join(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 
+    "news.db"
+)
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + DB_PATH
+
+PARSER_RULES = {
+    "ria": {
+        "url": "https://ria.ru/export/rss2/index.xml",
+        "rss": {
+            "title": "title", 
+            "description": "description", 
+            "url": "guid", 
+            "pub_dttm": "pubdate",
+        },
+        "news": {
+            "fulltext": {
+                "tag": "div",
+                "attrs": {
+                    "class": "article__text"
+                } 
+            },
+            "authors": {
+                "tag": "div",
+                "attrs": {
+                    "class": "article__author-name"
+                }
+            },
+            "keywords": {
+                "tag": "a",
+                "attrs": {
+                    "class": "article__tags-item"
+                },
+            },
+            "rubric": {
+                "tag": "meta",
+                "attrs": {
+                    "name": "analytics:rubric"
+                },
+                "get": "content"
+            }
+        }
     },
-    "news": {
-        "id": "TEXT", 
-        "fulltext": "TEXT", 
-        "etl_dttm": "TEXT"
+    "tass": {
+        "url": "http://tass.ru/rss/v2.xml",
+        "rss": {
+            "title": "title", 
+            "description": "description", 
+            "url": "guid", 
+            "pub_dttm": "pubdate",
+        },
+        "news": {
+            "fulltext": {
+                "tag": "p"
+            },
+            "authors": {
+                "tag": "div",
+                "attrs": {
+                    "class": "person-name"
+                } 
+            },
+            "keywords": {
+                "tag": "a",
+                "attrs": {
+                    "class": "tags__item"
+                }
+            }
+        }
+    },
+    "meduza": {
+        "url": "https://meduza.io/rss/all",
+        "rss": {
+            "title": "title", 
+            "description": "description", 
+            "url": "guid", 
+            "pub_dttm": "pubdate",
+        },
+        "news": {
+            "fulltext": {
+                "tag": "p",
+                "attrs": {"class": "SimpleBlock-p"}
+            },
+            "authors": {
+                "tag": "p",
+                "attrs": {"class": "MaterialNote-note_caption"}
+            },
+            "keywords": {
+                "tag": "meta",
+                "attrs": {"name": "keywords"},
+                "get": "content"
+            },
+            "rubric": {
+                "tag": "div",
+                "attrs": {
+                    "class": "Tag-root Tag-large Tag-gold"
+                }
+            }
+        }
+    },
+    "newsru": {
+        "url": "https://rss.newsru.com/all_news/",
+        "rss": {
+            "title": "title", 
+            "description": "description", 
+            "url": "guid", 
+            "pub_dttm": "pubdate",
+        },
+        "news": {
+            "fulltext": {
+                "tag": "p",
+                "attrs": {
+                    "class": "maintext"
+                }
+            },
+            "authors": {
+                "tag": "a",
+                "attrs": {
+                    "class": "article-author"
+                }
+            },
+            "keywords": {
+                "tag": "meta",
+                "attrs": {
+                    "name": "news_keywords"
+                },
+                "get": "content"
+            },
+            "rubric": {
+                "tag": "div",
+                "attrs": {
+                    "class": "main-caption"
+                }
+            }
+        }
     }
 }
-
-RELATIONS = [
-    [
-        {
-            "table": "rss",
-            "column": "id",
-            "is_multiple": False
-        },
-        {
-            "table": "news",
-            "column": "id",
-            "is_multiple": False
-        }
-    ]
-]
-
-RSS_SOURCES = [
-    "https://ria.ru/export/rss2/index.xml",
-    "http://tass.ru/rss/v2.xml",
-    "https://meduza.io/rss/all",
-    "https://rss.newsru.com/all_news/"
-]
